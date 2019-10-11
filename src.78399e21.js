7e801d8ac2380be2b3ef00f59224521daed76fef
Kelley van Evert <hello@kelleyvanevert.nl>
2019-10-12 00:11:18 +0200

    Updates

--- src.78399e21.js
+++ src.78399e21.js
@@ -35241,7 +35241,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52733" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58673" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
