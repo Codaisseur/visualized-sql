93ad72bc89a739fa718c63581bf308b60230a747
Kelley van Evert <hello@kelleyvanevert.nl>
2019-10-13 11:47:35 +0200

    added brackets, multiple join info, order by has asc/desc

--- src/CheatSheet/CheatSheet.jsx
+++ src/CheatSheet/CheatSheet.jsx
@@ -6,57 +6,68 @@ import "./CheatSheet.scss";
 export default function CheatSheet() {
   return (
     <div className="query">
-      <div className="box mb-1">
-        <div className="line mb-2">
-          <div className="clause mb-1">SELECT</div>
-          <div>
-            <div className="var mb-1">column(s)</div>
-            <div className="info mb-1">
-              You should leave this part to the end ^^, and until then, just
-              keep it at *
+      <div className="line mb-2">
+        <div className="box mb-1">
+          <div className="line mb-2">
+            <div className="clause mb-1">SELECT</div>
+            <div className="stack">
+              <div className="var mb-1">column(s)</div>
+              <div className="info mb-1">
+                You should leave this part to the end ^^, and until then, just
+                keep it at *
+              </div>
+            </div>
+          </div>
+          <div className="line">
+            <div className="clause mb-1">FROM</div>
+            <div className="stack">
+              <div className="var mb-1">table</div>
+              <div className="info" style={{ maxWidth: 220 }}>
+                Start here! This is the first important step, and for the while
+                being just use SELECT *
+              </div>
             </div>
           </div>
         </div>
-        <div className="line">
-          <div className="clause mb-1">FROM</div>
-          <div>
-            <div className="var mb-1">table</div>
-            <div className="info" style={{ maxWidth: 220 }}>
-              Start here! This is the first important step, and for the while
-              being just use SELECT *
-            </div>
-          </div>
+        <div className="info" style={{ maxWidth: 120 }}>
+          The core part of every SQL query. Everything else is optional
         </div>
-      </div>
-      <div className="info mb-2">
-        ▲ The core part of every SQL query. Everything else is optional
       </div>
       <div className="line mb-2">
-        <div>
-          <div className="clause mb-1">LEFT</div>
-          <div className="info mb-1" style={{ maxWidth: 180 }}>
-            Optional! Do you want to select rows from the FROM table if none in
-            the JOINed table match up?
-          </div>
-        </div>
-        <div className="clause mb-1">JOIN</div>
-        <div className="var mb-1">table</div>
-        <div className="line">
-          <div className="clause mb-1">ON</div>
-          <div>
-            <div className="var mb-1">joining equation</div>
-            <div className="info mb-1" style={{ maxWidth: 400 }}>
-              Used to decide whether each pair of rows, one from the FROM table,
-              and one from the JOINed table, match up. It's totally possibly for
-              multiple matches to happen on the same row, as is often the case
-              with a 1:N or N:N relationship.
+        <div className="box bracket mb-1">
+          <div className="line">
+            <div className="stack">
+              <div className="clause mb-1">LEFT</div>
+              <div className="info mb-1" style={{ maxWidth: 180 }}>
+                Optional! Do you want to select rows from the FROM table if none
+                in the JOINed table match up?
+              </div>
+            </div>
+            <div className="line">
+              <div className="clause mb-1">JOIN</div>
+              <div className="var mb-1">table</div>
+            </div>
+            <div className="line">
+              <div className="clause mb-1">ON</div>
+              <div className="stack">
+                <div className="var mb-1">joining equation</div>
+                <div className="info" style={{ maxWidth: 400 }}>
+                  Used to decide whether each pair of rows, one from the FROM
+                  table, and one from the JOINed table, match up. It's totally
+                  possibly for multiple matches to happen on the same row, as is
+                  often the case with a 1:N or N:N relationship.
+                </div>
+              </div>
             </div>
           </div>
+        </div>
+        <div className="info" style={{ maxWidth: 140 }}>
+          You can in fact join as many tables as you want!
         </div>
       </div>
       <div className="line mb-2">
         <div className="clause mb-1">WHERE</div>
-        <div>
+        <div className="stack">
           <div className="var mb-1">boolean filter expression</div>
           <div className="info mb-1" style={{ maxWidth: 400 }}>
             Just like in JavaScript, you can combine multiple boolean
@@ -67,7 +78,7 @@ export default function CheatSheet() {
         </div>
       </div>
       <div className="line mb-2">
-        <div>
+        <div className="stack">
           <div className="line">
             <div className="clause mb-1">GROUP BY</div>
             <div className="var mb-1">column(s)</div>
@@ -84,10 +95,18 @@ export default function CheatSheet() {
       </div>
       <div className="line mb-2">
         <div className="clause mb-1">ORDER BY</div>
-        <div>
-          <div className="var mb-1">column(s)</div>
+        <div className="stack">
+          <div className="bracket mb-1">
+            <div className="line">
+              <div className="var">column</div>
+              <div className="stack">
+                <div className="clause half mr-0">ASC</div>
+                <div className="clause half mr-0">DESC</div>
+              </div>
+            </div>
+          </div>
           <div className="info mb-1" style={{ maxWidth: 240 }}>
-            You can add multiple columns here, separated by comma's. What effect
+            You can add multiple of these, separated by comma's. What effect
             does this have?
           </div>
         </div>
